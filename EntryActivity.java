a9f54e55e919beeb6cec77ed11223bfa2c8ee1b0
Zhnkld <zhaks.yerkin@gmail.com>
2019-01-09 01:44:13 +0600

    entry

--- EntryActivity.java
+++ EntryActivity.java
@@ -7,31 +7,63 @@ import android.support.v7.app.AppCompatActivity;
 
 public class EntryActivity extends AppCompatActivity {
 
+
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_entry);
         showAuth();
+
+
     }
 
     private void showAuth() {
-        AuthFragment fragment = new AuthFragment();
+        AuthFragment fragment1 = new AuthFragment();
         getSupportFragmentManager()
                 .beginTransaction()
-                .add(R.id.entry_container, fragment)
+                .add(R.id.entry_container, fragment1)
                 .addToBackStack(null)
                 .commit();
     }
 
-    private void showMain() {
-        // TODO(“show main fragment”)
+    public void showMain() {
+        MainFragment fragment2 = new MainFragment();
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.entry_container, fragment2)
+                .commit();
+
     }
 
     public static class AuthFragment extends Fragment {
 
         public final void onAuthSuccess() {
-            // TODO(“show main fragment”)
+                showMain();
         }
+
+        }
+
+
+
     }
 
-    public static class MainFragment extends Fragment { }
+    public static class MainFragment extends Fragment {
+
+        public void onBackPressed(){
+            onBackPressed();
+        }
+
+
+
+
+
+
+    @Override
+    public void onBackPressed() {
+            super.onBackPressed();
+    }
+
+
+
+
+
 }
